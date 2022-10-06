e8217ea1a874cc665ccd7d075873945fc25b208f
NGchang <b24111624@hotmail.com>
2022-10-06 14:18:22 -0700

    1. init project

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	logger "github.com/sirupsen/logrus"
+)
+
+func main() {
+	// Init log
+	logger.SetLevel((logger.DebugLevel))
+	logger.Info("Starting basic-byte-compressor")
+
+	// TODO: add implementations
+}
