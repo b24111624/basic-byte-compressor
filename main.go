3174285f9d887dbb869fe96427ce615b0c93b4b4
NGchang <b24111624@hotmail.com>
2022-10-07 02:02:45 -0700

    1. update compressor process 2. output to console

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"compress/gzip"
 	"io"
 	"os"
@@ -21,13 +22,30 @@ func main() {
 	defer rc.Close()
 
 	// Prepare output file
-	f, _ := os.Create("./gzip_file.gz")
-	zw := gzip.NewWriter(f)
-	defer zw.Close()
+	var buf bytes.Buffer
+	zw := gzip.NewWriter(&buf)
 
-	// Compress data
+	// Compress data to buffer
 	_, err := io.Copy(zw, rc)
 	if err != nil {
 		logger.WithField("err", err).Fatal("io.Copy failed")
 	}
+
+	if err := zw.Close(); err != nil {
+		logger.WithField("err", err).Fatal("zw.close failed")
+	}
+
+	zr, err := gzip.NewReader(&buf)
+	if err != nil {
+		logger.WithField("err", err).Fatal("gzip.NewReader failed")
+	}
+
+	if _, err := io.Copy(os.Stdout, zr); err != nil {
+		logger.WithField("err", err).Fatal("io.Copy to stdout failed")
+	}
+
+	if err := zr.Close(); err != nil {
+		logger.WithField("err", err).Fatal("zr.Close failed")
+	}
+
 }
