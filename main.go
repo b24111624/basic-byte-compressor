d57bebb15ddbc9ed2d71805a2b03f25bcfe5521f
NGchang <b24111624@hotmail.com>
2022-10-06 18:00:26 -0700

    1. implement basic ios 2. compress example strings to gz file

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"compress/gzip"
+	"io"
+	"os"
+	"strings"
+
 	logger "github.com/sirupsen/logrus"
 )
 
@@ -9,5 +14,20 @@ func main() {
 	logger.SetLevel((logger.DebugLevel))
 	logger.Info("Starting basic-byte-compressor")
 
-	// TODO: add implementations
+	// Prepare testing byte datas
+	testData := "compressed data compressed data compressed data"
+
+	rc := io.NopCloser(strings.NewReader(testData))
+	defer rc.Close()
+
+	// Prepare output file
+	f, _ := os.Create("./gzip_file.gz")
+	zw := gzip.NewWriter(f)
+	defer zw.Close()
+
+	// Compress data
+	_, err := io.Copy(zw, rc)
+	if err != nil {
+		logger.WithField("err", err).Fatal("io.Copy failed")
+	}
 }
